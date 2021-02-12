65e5be98af4a16507d98f10317bd9bf73f58c0a9
Lucas <lkubok@gmail.com>
2021-02-12 16:52:11 +0100

    F/13: added Tag controller for adding and getting tag

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :collaterals, only: [:show, :index, :create]
+      resources :tags, only: [:show, :index, :create]
     end
   end
 end
+
+
+  # devise_for :tags
+  #   get 'api/v1/tags/index'
+  #   post 'api/v1/tags/add'
+  # end
+
