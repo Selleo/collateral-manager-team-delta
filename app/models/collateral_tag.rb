5ff4c30480b0525994b7f31212d75256c1c65a02
rinkachi <japonics.email@gmail.com>
2021-02-16 19:33:19 +0100

    Feature/26 - work on leads

--- app/models/collateral_tag.rb
+++ app/models/collateral_tag.rb
@@ -2,4 +2,7 @@ class CollateralTag < ApplicationRecord
   validates :collateral_id, presence: true
   validates :tag_id, presence: true
   validates :weight, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10 }
+
+  belongs_to :tag
+  belongs_to :collateral
 end
