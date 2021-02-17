766be28ac86fcb323f809a16b51691fb5d037cfe
alan0z <alanlipa@gmail.com>
2021-02-17 17:21:10 +0100

    25 - zmiany admin

--- app/services/leads/show_service.rb
+++ app/services/leads/show_service.rb
@@ -1,18 +1,28 @@
-module Collaterals
+module leads
   class ShowService
-    def initialize(collateral_id)
-      @collateral_id = collateral_id
+    def initialize(lead_id)
+      @lead_id = lead_id
     end
 
-    def self.call(collateral_id)
-      new(collateral_id).call
+    def self.call(lead_id)
+      new(lead_id).call
     end
 
     def call
-      Collateral.find(collateral_id)
+      lead = Lead.find(lead_id)
+      search_alghoritm_start(lead)
     end
 
     private
-    attr_reader :collateral_id
+    attr_reader :lead_id
+
+    # def search_alghoritm_start(lead)
+    #   LeadTag.find_by(lead_id: lead_id) do |lt|
+    #     CollateralTags.find_by(tag_id: lt.tag_id).order("weight DESC") |ct| do 
+
+    #     end
+    #   end
+    # end
   end
 end
+
