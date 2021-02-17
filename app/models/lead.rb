99ada2c8ab77abaf22ce065c2fea39af5772843d
alan0z <alanlipa@gmail.com>
2021-02-17 19:26:00 +0100

    39 algorytm

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,6 +1,7 @@
 class Lead < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  has_many :tags, through => :lead_tags
   has_many :lead_tags
 
   accepts_nested_attributes_for :lead_tags
