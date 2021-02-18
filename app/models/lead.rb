dfd7abc105c6a623e96673e5c0f9c501543983e7
Kamil Korczynski <korczas+github@gmail.com>
2021-02-18 18:45:36 +0100

    fix: Lead model tags relation

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,7 +1,6 @@
 class Lead < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  has_many :tags
   has_many :lead_tags
   has_many :tags, through: :lead_tags
 
